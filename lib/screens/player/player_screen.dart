9e6d99638a0bf6d72ffdbfa1223aa98f5c7a97ce
Fued Gabriel <fuedgabriel@hotmail.com>
2023-11-03 02:23:44 -0300

    Details

---
+++ lib/screens/player/player_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class playervideo extends StatelessWidget {
+  const playervideo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
